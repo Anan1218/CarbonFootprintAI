a929bdd7eea947e4e5f3591b8ac4b30fe1bfbd3c
Anan <mengan.wang@gmail.com>
2021-01-21 22:30:42 -0800

    frontend setup

--- aarush.py
+++ aarush.py
@@ -1,1 +1,16 @@
-hi guys
+# hi guys
+# hello there
+# aarush.py
+# since: 1.21.2021
+
+# bash: pip install clarifai-grpc
+
+ver = grpc._version_
+
+print(ver)
+# from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+
+
+print("i love anan <3")
+
+# im so confused
